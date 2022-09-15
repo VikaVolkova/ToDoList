c17f00b6303a45ec93cef6a2166d555ed0feb7c8
v.volkova <v.volkova@godeltech.com>
2022-09-15 16:11:57 +0300

    Add-user-endpoints

--- server/validation/user.js
+++ server/validation/user.js
@@ -1,9 +1,15 @@
 const Joi = require("joi");
 
-const userSchema = Joi.object({
+const userSchemaRegister = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().min(3).max(200).email().required(),
   password: Joi.string().min(6).max(1024).required(),
 });
 
-exports.userSchema = userSchema;
+const userSchemaLogin = Joi.object({
+  email: Joi.string().min(3).max(200).email().required(),
+  password: Joi.string().min(6).max(1024).required(),
+});
+
+exports.userSchemaRegister = userSchemaRegister;
+exports.userSchemaLogin = userSchemaLogin;
