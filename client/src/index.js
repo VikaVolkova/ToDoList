51248075d2ed3771df9f3a5b991b691538ecae97
Viktoriia Volkova <viktoriafeldman26@gmail.com>
2022-09-21 19:25:37 +0300

    Add and configure Redux store

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import todosReducer from "./features/todosSlice";
+
+const store = configureStore({
+  reducer: {
+    todosState: todosReducer,
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
