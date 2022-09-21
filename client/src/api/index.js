7373e6a9c378460bb93219afb88fa770bb37c930
Viktoriia Volkova <viktoriafeldman26@gmail.com>
2022-09-21 20:42:53 +0300

    Create add todo endpoint

---
+++ client/src/api/index.js
@@ -0,0 +1,17 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const baseURL = "http://localhost:5000/api/";
+
+export const todosAdd = createAsyncThunk(
+  "todos/todosAdd",
+  async (todo, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(`${baseURL}todos`, todo);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue(error.response?.data);
+    }
+  }
+);
--- client/src/components/AddTodoForm/index.jsx
+++ client/src/components/AddTodoForm/index.jsx
@@ -3,17 +3,45 @@ import { TextField, Button } from "@mui/material";
 import { Send } from "@mui/icons-material";
 import s from "./index.module.css";
 import palette from "../../palette";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { todosAdd } from "../../api";
 
 const AddTodoForm = () => {
+  const dispatch = useDispatch();
+  const [todo, setTodo] = useState({
+    name: "",
+    isComplete: false,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(todosAdd(todo));
+    console.log(todo);
+    setTodo({
+      name: "",
+      isComplete: false,
+    });
+  };
+
   return (
     <>
-      <form className={s.form} noValidate autoComplete="off">
+      <form
+        className={s.form}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
         <TextField
           id="add-todo"
           label="addToDO"
           size="small"
           autoFocus
           fullWidth
+          value={todo.name}
+          onChange={(e) =>
+            setTodo({ ...todo, name: e.target.value, date: new Date() })
+          }
         />
         <Button
           variant="contained"
